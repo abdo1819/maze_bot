069c66258a9874c74e9b7c532bc6c676fda5befa
abdo ragab hashem <abdo14hashem15@gmail.com>
2018-02-18 16:57:46 +0200

    robot8
    complate lcd with avr 8

---
+++ avr code/robot .2/ROBOT8/ROBOT8/DISTANCE_DETECTOR.c
@@ -0,0 +1,97 @@
+/*
+ * DISTANCE_DETECTOR.c
+ *
+ * Created: 1/30/2018 11:14:51 AM
+ *  Author: HMS
+ */ 
+
+#include "DISTANCE_DETECTOR.h"
+#include "LCD.h"
+#include <avr/interrupt.h>
+#include <stdio.h>
+#include <string.h>
+volatile float secs;
+volatile uint16_t over_flow=0;
+uint8_t trig_val =TRIG;
+
+
+volatile  char flag=1;
+
+void start( uint8_t trig)
+{  //init trig position , interrupt type and interrupt pin
+	 
+	DDRD=0;
+	PORTD=0;
+	// determine the interrupt pin that will be used by knowing which trig will be send
+	//case of int1
+		 MCUCR|=(1<<ISC10);
+		 GICR|=(1<<INT1);
+		 DDRD|=(1<<trig);
+		 trig_val=trig;//pass trig to trig signal function
+		 flag=1;
+	sei();//enable global interrupt
+}
+
+
+ uint16_t MEASURE_IN_CM()
+{
+	TRIG_SIGNAL();
+	//timer 1 no prescale lowest time =1/million=.001msec and max =1/million*2^16=65ms
+	//lowest time from hc sr04 =150 u sec=.15 m sec ****data sheet****
+	//max time from hc sr04 =25 m sec or 18 m sec(will not reach it)****data sheet****
+	// no obstacle time= 38 m sec
+ while((flag==0)&&(secs==0)){
+	 
+	 if (over_flow>=1)
+	 { 
+		 
+		 return 0;//in case of wrong connection or any problems (TIMER VALUE WIll NOT OVER FLOW ) ZERO RETURN IN CASE OF OVER FLOW
+		 }
+	 }
+	 
+		 _delay_ms(50);
+		 TCCR1B=0;
+		 DDRD&=~(1<<trig_val);
+
+
+ 	return (secs/2)*343/10000;//CONVERSION MISSED XD
+}
+void TRIG_SIGNAL( )
+{
+	//send trig pulse
+	PORTD|=(1<<trig_val);
+	_delay_us(10);
+	PORTD&=~(1<<trig_val);
+	_delay_us(750);
+	
+}
+
+ISR(INT1_vect)
+{
+	if (flag==1){
+	TCCR1B|=(1<<CS10);
+	TIMSK|=(1<<TOIE1);
+	
+	TCNT1=0;
+	flag=0;
+	secs=0;
+	}
+	else
+	 {   secs=TCNT1;
+		 TCCR1B=0;
+		  
+		 flag=1;
+	     over_flow=0;
+		TCNT1=0;
+	}
+}
+
+ISR(TIMER1_OVF_vect)
+{
+	over_flow=over_flow+1;
+	TCNT1=0;
+	TCCR1B|=(1<<CS10);
+	DDRC|=(1<<2);
+	PORTC^=(1<<2);;//over flow test
+	
+}
---
+++ avr code/robot .2/ROBOT8/ROBOT8/main.c
@@ -0,0 +1,38 @@
+/*
+ * ULTRA_SONIC.c
+ *
+ * Created: 1/30/2018 7:45:32 AM
+ * Author : HMS
+ */ 
+
+#include <avr/io.h>
+ 
+#include <stdio.h>
+#define F_CPU 1000000UL
+#include "DISTANCE_DETECTOR.h"
+#include <util/delay.h>
+#include "lcd.h"
+
+int main(void)
+{     
+	  uint16_t RIGHT=0;
+	  uint16_t LEFT=0;
+	DDRD=0;
+	PORTD|=1<<PD3;
+	
+	
+
+    while (1) 
+    {
+		
+		start(PORTD1);
+		LEFT=MEASURE_IN_CM();
+		
+		_delay_ms(50);		 
+		 init();
+		 display_int(LEFT);
+		 _delay_ms(10);
+		  
+    }
+
+}
